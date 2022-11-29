8778faecebe30d93bd8c53d5ef196cdd628b7ee5
Gabriel Martins <78751933+DeskFanzin@users.noreply.github.com>
2022-11-29 14:32:21 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from classes.process import Process
 
 
 def main():
-    devices = []
+    ##Variaveis de uso do sistema##
+    devices = [] 
     processes = []
     cpu_fraction = 0
 
     with open("./entrada/entrada_es.txt", "r") as file:
+        ## retira do arquivo as informacoes da primeira linha ##
         cpu_fraction, qnt_devices = file.readline().strip().split("|")
         cpu_fraction = int(cpu_fraction)
         qnt_devices = int(qnt_devices)
 
+        ##Adicionando os Dispositivos na lista ##
         for _ in range(qnt_devices):
             name, simultaneous_access, operating_time = file.readline().strip().split("|")
             devices.append(
                 Device(name, int(simultaneous_access),
                        int(operating_time))
             )
-            
+        ##Adicionando os Processos na lista ##
         for line in file:
             PID, operating_time, probability_of_executing_a_device = line.strip().split("|")
             processes.append(
@@ -27,9 +30,11 @@ def main():
                         int(probability_of_executing_a_device))
             )
 
+    ## Rodando o Sistema Operacional ##
     os = OperatingSystem(processes, devices, cpu_fraction)
     os.run()
 
 
 if __name__ == "__main__":
     main()
+
